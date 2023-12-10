5b782e6696438958403a441dedc07e9acc6543b1
ripon4521 <riponakondo4521@gmail.com>
2023-12-10 12:54:51 +0600

    create a react app and routing system

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      fontFamily:{
+       'jost': ['Jost', 'sans-serif'],
+       'popins': ['Poppins', 'sans-serif']
+      }
+    },
+  },
+  plugins: [],
+}
+
