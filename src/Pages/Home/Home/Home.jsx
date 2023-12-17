af93518fd49b264134851922a89b880e1c5ff9a5
ripon4521 <riponakondo4521@gmail.com>
2023-12-18 05:02:23 +0600

    create a responsive quote section

--- src/Pages/Home/Home/Home.jsx
+++ src/Pages/Home/Home/Home.jsx
@@ -2,6 +2,7 @@ import Banner from "../Banner/Banner";
 import Gallery from "../Gallery/Gallery";
 import Grantee from "../Grantee/Grantee";
 import Navbar from "../Navbar/Navbar";
+import Renul from "../RenaulSection/Renul";
 import TopHeader from "../TopHeader/TopHeader";
 import TrustUs from "../TrustUs/TrustUs";
 
@@ -15,6 +16,7 @@ const Home = () => {
             <Grantee></Grantee>
             <TrustUs></TrustUs>
             <Gallery></Gallery>
+            <Renul></Renul>
         </div>
     );
 };
---
+++ src/Pages/Home/RenaulSection/Renul.jsx
@@ -0,0 +1,22 @@
+import { FaPhone } from 'react-icons/fa';
+
+
+const Renul = () => {
+   
+    return (
+        <div className='mt-20'>
+           <div className="hero md:h-28 lg:h-20" style={{backgroundImage: 'url(https://i.ibb.co/s6QvdZG/Rectangle-36.png)'}}>
+  <div className=" "></div>
+  <div className="hero-content ">
+    <div className=" flex gap-5 lg:gap-10 items-center">
+      <h1 className=" lg:text-3xl text-2xl font-jost font-semibold text-white">Bathtub Reglazing: Affordable and Sustainable Bathroom Renewal</h1>
+      
+      <button className="lg:px-4 px-3 flex items-center gap-1 py-2 bg-white rounded-xl font-popins font-semibold text-[#095090] text-xl"><FaPhone></FaPhone> Get a Quote</button>
+    </div>
+  </div>
+</div>
+        </div>
+    );
+};
+
+export default Renul;
